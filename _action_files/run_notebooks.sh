6dd7417fb9a2abb8c3a0ff212892686317101587
srcnor <73624874+srcnor@users.noreply.github.com>
2021-01-30 21:57:09 -0600

    Create run_notebooks.sh

---
+++ _action_files/run_notebooks.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -e
+cd $(dirname "$0")/..
+cd _notebooks/
+
+ERRORS=""
+
+papermill --kernel python3 "2021-01-30-herd-immunity-ii.ipynb" "2021-01-30-herd-immunity-ii.ipynb" 
+
+# Emit Errors If Exists So Downstream Task Can Open An Issue
+if [ -z "$ERRORS" ]
+then
+    echo "::set-output name=error_bool::false"
+else
+    echo "These files failed to update properly: ${ERRORS}"
+    echo "::set-output name=error_bool::true"
+    echo "::set-output name=error_str::${ERRORS}"
+fi
