edc63fe97e48ebee1c74d880d56438a0ed7ea9ec
srcnor <73624874+srcnor@users.noreply.github.com>
2021-02-11 22:33:58 -0600

    Adding herd-immunity-iii to auto update

--- _action_files/run_notebooks.sh
+++ _action_files/run_notebooks.sh
@@ -6,6 +6,8 @@ cd _notebooks/
 ERRORS=""
 
 papermill --kernel python3 "2021-01-30-herd-immunity-ii.ipynb" "2021-01-30-herd-immunity-ii.ipynb" 
+papermill --kernel python3 "2021-02-08-herd-immunity-iii.ipynb" "2021-02-08-herd-immunity-iii.ipynb" 
+
 
 # Emit Errors If Exists So Downstream Task Can Open An Issue
 if [ -z "$ERRORS" ]
